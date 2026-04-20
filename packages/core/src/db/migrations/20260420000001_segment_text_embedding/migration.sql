c4f14463c627fb9cfaa98da92c698b11f5585cf7
Nick Crews <nicholas.b.crews@gmail.com>
2026-04-20 08:59:03 -0800

    compact migrations

--- packages/core/src/db/migrations/20260420000001_segment_text_embedding/migration.sql
+++
@@ -1,3 +0,0 @@
--- Add 384-dim text embedding column to segments for semantic search
--- (all-MiniLM-L6-v2 via @xenova/transformers).
-ALTER TABLE "segments" ADD COLUMN "text_embedding" vector(384);
